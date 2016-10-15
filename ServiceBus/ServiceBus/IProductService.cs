ef0f74ab894923f4e4318d765c5a52c420d760d5
MartinCernohorsky <martin.cernohorsky.st@vsb.cz>
2016-10-15 21:28:24 +0200

    Add product
    This commit contains two methods which are supposed to add new product
    into product table. Since I don't know better solution at this moment
    both methods returns Result datatype.
    
    Both methods have been sucessfully tested. During the test I found out
    that using default constructor of Guid class returns id made of zeros so
    it is better to use static method Guid.newGuid() .

--- ServiceBus/ServiceBus/IProductService.cs
+++ ServiceBus/ServiceBus/IProductService.cs
@@ -1,5 +1,6 @@
 ﻿using System.ServiceModel;
 using System;
+using System.Web.Services;
 
 namespace ServiceBus
 {
@@ -24,5 +25,23 @@ namespace ServiceBus
         /// <returns>collection of all products</returns>
         [OperationContract]
         SharedLibs.DataContracts.Products GetAllProducts();
+
+        /// <summary>
+        /// This method adds product into datasource
+        /// </summary>
+        /// <param name="name">Name of a new product</param>
+        /// <param name="price">Price of a new product</param>
+        /// <param name="guid">ID of a new prodcut</param>
+        /// <returns>Result object</returns>
+        [OperationContract (Name = "AddProduct")]
+        SharedLibs.DataContracts.Result AddProduct(string name, double price, Guid guid);
+
+        /// <summary>
+        /// This method adds product into the datasource
+        /// </summary>
+        /// <param name="product">Product object</param>
+        /// <returns>Result object</returns>
+        [OperationContract (Name = "AddProductByObject")]
+        SharedLibs.DataContracts.Result AddProduct(SharedLibs.DataContracts.Product product);
     }
 }
