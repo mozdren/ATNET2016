d398a5ccc0861c1664f03ac88f7815f78ac6c8c1
Marek Bily <marekbily@gmail.com>
2016-10-23 17:28:05 +0200

    Add ICampaignService and CampaignService (waiting for implementation)

---
+++ ServiceBus/ServiceBus/CampaignService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using SharedLibs.DataContracts;
+
+namespace ServiceBus
+{
+    public class CampaignService : ICampaignService
+    {
+        public SharedLibs.DataContracts.Campaign GetCampaign(Guid guid)
+        {
+            //before continuing is necessary to add Campaign to ServiceBusDatabaseEntities
+            throw new NotImplementedException();
+        }
+
+        public SharedLibs.DataContracts.Campaigns GetAllCampaign()
+        {
+            //before continuing is necessary to add Campaign to ServiceBusDatabaseEntities
+            throw new NotImplementedException();
+        }
+
+        public SharedLibs.DataContracts.Result AddCampaign(Campaign campaign)
+        {
+            //before continuing is necessary to add Campaign to ServiceBusDatabaseEntities
+            throw new NotImplementedException();
+        }
+
+        public SharedLibs.DataContracts.Result AddCampaign(Guid guid, string name, double discount)
+        {
+            //before continuing is necessary to add Campaign to ServiceBusDatabaseEntities
+            throw new NotImplementedException();
+        }
+
+        public Result EditCampaign(Guid guid, string name, double discount)
+        {
+            //before continuing is necessary to add Campaign to ServiceBusDatabaseEntities
+            throw new NotImplementedException();
+        }
+
+        public Result DeleteCampaign(Guid guid)
+        {
+            //before continuing is necessary to add Campaign to ServiceBusDatabaseEntities
+            throw new NotImplementedException();
+        }
+    }
+}
