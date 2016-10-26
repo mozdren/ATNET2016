8452728a7e0bf5e4455cf20dc074fe8254197fb5
Karel Mozdren <mozdren@gmail.com>
2016-10-26 13:37:17 +0200

    Merge - From mbily branch

---
+++ ServiceBus/ServiceBus/CampaignService.svc.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using SharedLibs.DataContracts;
+
+namespace ServiceBus
+{
+    public class CampaignService : ICampaignService
+    {
+        public SharedLibs.DataContracts.Campaign GetCampaign(Guid guid)
+        {
+            //before continuing is necessary to add Campaign to ServiceBusDatabaseEntities
+            return new SharedLibs.DataContracts.Campaign
+            {
+                Result = Result.Fatal("Not Implemented")
+            };
+        }
+
+        public SharedLibs.DataContracts.Campaigns GetAllCampaigns()
+        {
+            //before continuing is necessary to add Campaign to ServiceBusDatabaseEntities
+            return new SharedLibs.DataContracts.Campaigns
+            {
+                Result = Result.Fatal("Not Implemented")
+            };
+        }
+
+        public SharedLibs.DataContracts.Result AddCampaign(Campaign campaign)
+        {
+            //before continuing is necessary to add Campaign to ServiceBusDatabaseEntities
+            return Result.Fatal("Not Implemented");
+        }
+
+        public SharedLibs.DataContracts.Result AddCampaign(Guid guid, string name, double discount)
+        {
+            //before continuing is necessary to add Campaign to ServiceBusDatabaseEntities
+            return Result.Fatal("Not Implemented");
+        }
+
+        public Campaign EditCampaign(Guid guid, string name, double discount)
+        {
+            //before continuing is necessary to add Campaign to ServiceBusDatabaseEntities
+            return new SharedLibs.DataContracts.Campaign
+            {
+                Result = Result.Fatal("Not Implemented")
+            };
+        }
+
+        public Result DeleteCampaign(Guid guid)
+        {
+            //before continuing is necessary to add Campaign to ServiceBusDatabaseEntities
+            return Result.Fatal("Not Implemented");
+        }
+    }
+}
---
+++ ServiceBus/ServiceBus/ICampaignService.cs
@@ -0,0 +1,65 @@
+﻿using System.ServiceModel;
+using System;
+using System.Web.Services;
+
+namespace ServiceBus
+{
+    /// <summary>
+    /// This is a campaign service, which should serve as an entrypoint for everything that has
+    /// something directly in common with campaigns.
+    /// </summary>
+    [ServiceContract]
+    public interface ICampaignService
+    {
+        /// <summary>
+        /// Returns campaign with specific Guid
+        /// </summary>
+        /// <param name="guid">guid of a campaign</param>
+        /// <returns>requested campaign</returns>
+        [OperationContract]
+        SharedLibs.DataContracts.Campaign GetCampaign(Guid guid);
+
+        /// <summary>
+        /// Returns all campaigns
+        /// </summary>
+        /// <returns>collection of all campaigns</returns>
+        [OperationContract]
+        SharedLibs.DataContracts.Campaigns GetAllCampaigns();
+
+        /// <summary>
+        /// Adds campaign into database
+        /// </summary>
+        /// <param name="guid">ID of a campaign</param>
+        /// <param name="name">Name of a campaign</param>
+        /// <param name="discount">Discount of a campaign</param>
+        /// <returns>Result object</returns>
+        [OperationContract]
+        SharedLibs.DataContracts.Result AddCampaign(Guid guid, string name, double discount);
+
+        /// <summary>
+        /// Adds campaign into database
+        /// </summary>
+        /// <param name="campaign">Campaign object</param>
+        /// <returns>Result object</returns>
+        [OperationContract]
+        SharedLibs.DataContracts.Result AddCampaign(SharedLibs.DataContracts.Campaign campaign);
+
+        /// <summary>
+        /// Edits campaign with specific Guid
+        /// </summary>
+        /// <param name="guid">ID of a campaign</param>
+        /// <param name="name">New name for a campaign</param>
+        /// <param name="discount">New discount of a campaign</param>
+        /// <returns>Edited campaign</returns>
+        [OperationContract]
+        SharedLibs.DataContracts.Campaign EditCampaign(Guid guid, string name, double discount);
+
+        /// <summary>
+        /// Delete campaign from database
+        /// </summary>
+        /// <param name="guid">ID of a campaign</param>
+        /// <returns>Result object</returns>
+        [OperationContract]
+        SharedLibs.DataContracts.Result DeleteCampaign(Guid guid);
+    }
+}
