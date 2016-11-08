a85027516c811cf087c20a3ae55bc2a62e1a1b59
MartinCernohorsky <martin.cernohorsky.st@vsb.cz>
2016-11-08 22:36:05 +0100

    New DTO objects
    New DTOs created solely to mirror context model.
    
    There is also enum ProductTypes created due to comment in previous
    commit. I do not know how to benifit from it now but I would like to
    leave it there if you don't mind.

---
+++ ServiceBus/SharedLibs/DataContracts/StorageItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibs.DataContracts
+{
+    /// <summary>
+    /// Datacontract for storage items
+    /// </summary>
+    [DataContract]
+    public class StorageItem
+    {
+        //TODO: Fill according to db contex.
+    }
+}
---
+++ ServiceBus/SharedLibs/Enums/ProductTypes.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibs.Enums
+{
+    /// <summary>
+    /// Categories for product. Each product has to belong to one category
+    /// </summary>
+    [DataContract]
+    public enum ProductTypes
+    {
+        /// <summary>
+        /// Product doesn't have special category
+        /// </summary>
+        [EnumMember]
+        Miscellaous = 0,
+
+        /// <summary>
+        /// Do it yourself category
+        /// </summary>
+        [EnumMember]
+        DIY,
+
+        /// <summary>
+        /// Stuff related to computers and IT in general
+        /// </summary>
+        [EnumMember]
+        Computers,
+
+        /// <summary>
+        /// All kinds electronic appliances except IT stuff
+        /// </summary>
+        [EnumMember]
+        Electronics,
+
+        /// <summary>
+        /// Clothes, shoes, jewelery
+        /// </summary>
+        [EnumMember]
+        Cloathing,
+
+        /// <summary>
+        /// Books, magazines, e-books and similiar content
+        /// </summary>
+        [EnumMember]
+        Books,
+
+        /// <summary>
+        /// Equipment for sport
+        /// </summary>
+        [EnumMember]
+        Sports,
+
+        /// <summary>
+        /// Category 18+
+        /// </summary>
+        [EnumMember]
+        Adults
+
+    }
+}
