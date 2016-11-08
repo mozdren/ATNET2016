0d480557049274cdba25bef5cdbb767a8289343f
MartinCernohorsky <martin.cernohorsky.st@vsb.cz>
2016-11-08 22:45:46 +0100

    Revert "Revert "New DTO objects""
    This reverts commit 7bd351034c240b28cd76f00c1f112b5ca1a26af1.

---
+++ ServiceBus/SharedLibs/DataContracts/ProductType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibs.DataContracts
+{
+    /// <summary>
+    /// Datacontract for product type
+    /// </summary>
+    [DataContract]
+    public class ProductType
+    {
+        /// <summary>
+        /// Identifier of the product type
+        /// </summary>
+        [DataMember]
+        public Guid ID { get; set; }
+
+        /// <summary>
+        /// Name of a product type
+        /// </summary>
+        [DataMember]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// All products of this product type
+        /// </summary>
+        [DataMember]
+        public List<Product> Products { get; set; }
+
+        /// <summary>
+        /// All campaigns aimed at this product
+        /// </summary>
+        [DataMember]
+        public List<Campaign> Campaings { get; set; }
+    }
+}
---
+++ ServiceBus/SharedLibs/DataContracts/Reservation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace SharedLibs.DataContracts
+{
+    /// <summary>
+    /// Datacontract for reserved products
+    /// </summary>
+    [DataContract]
+    public class Reservation : DTO
+    {
+        //TODO: Fill according to db contex.
+    }
+}
