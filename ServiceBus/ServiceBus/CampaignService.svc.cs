58ec4c02325f8a448310a29401111ca009046c04
petrborzutzky <borzutzky@gmail.com>
2016-12-04 09:10:18 +0100

    Add files via upload

--- ServiceBus/ServiceBus/CampaignService.svc.cs
+++ ServiceBus/ServiceBus/CampaignService.svc.cs
@@ -8,7 +8,7 @@ namespace ServiceBus
         public Campaign GetCampaign(Guid guid)
         {
             //before continuing is necessary to add Campaign to ServiceBusDatabaseEntities
-            return new SharedLibs.DataContracts.Campaign
+            return new Campaign
             {
                 Result = Result.Fatal("Not Implemented")
             };
@@ -31,8 +31,29 @@ namespace ServiceBus
 
         public Result AddCampaign(Guid guid, string name, double discount)
         {
-            //before continuing is necessary to add Campaign to ServiceBusDatabaseEntities
-            return Result.Fatal("Not Implemented");
+            try
+            {
+
+                if ((!string.IsNullOrWhiteSpace(name) && name.Length <= 50) && discount >= 0)
+                {
+                    using (var context = new EntityModels.ServiceBusDatabaseEntities())
+                    {
+
+                        context.Campaigns.Add(new EntityModels.Campaign() { Id=guid, Name = name, Discount=discount});
+                        context.SaveChanges();
+
+                        return Result.SuccessFormat("Product {0} | {1} has been added", guid, name);
+                    }
+                }
+                else
+                {
+                    return Result.Error("Provided parameters are unacceptable.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result.FatalFormat("ProductService.AddProduct Exception: {0}", ex.Message);
+            }
         }
 
         public Campaign EditCampaign(Guid guid, string name, double discount)
